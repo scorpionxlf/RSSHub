bb373fa11c041efa43bf892dd88b496fd7e275e1
scorpionxlf <421342241@qq.com>
2025-12-24 19:48:30 +0800

    Update namespace.ts

--- lib/routes/1lou/namespace.ts
+++ lib/routes/1lou/namespace.ts
@@ -2,7 +2,7 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: 'BT 之家 1LOU 站',
-    url: '1lou.me',
+    url: '1lou.one',
     categories: ['multimedia'],
     description: '',
     lang: 'zh-CN',
